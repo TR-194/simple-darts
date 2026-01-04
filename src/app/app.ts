a06fe6cdb51402bd2cbc917ecc69873c65fa97fe
Timon Roessler <roesslertimon@web.de>
2026-01-04 17:38:47 +0100

    scoreboard: erste logik überlegungen

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ScoreInput } from './features/game-501/components/score-input/score-input';
+import { Scoreboard } from './features/game-501/components/scoreboard/scoreboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ScoreInput],
+  imports: [RouterOutlet, Scoreboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/features/game-501/components/scoreboard/scoreboard.ts
+++ src/app/features/game-501/components/scoreboard/scoreboard.ts
@@ -8,6 +8,44 @@ import { Component } from '@angular/core';
 })
 export class Scoreboard {
   players: player[] = [];
+
+
+  createSinglesArray(): number[]{
+    let i = 0;
+    let singles = [];
+    for (i = 1; i <= 20; i++) {
+      singles.push(i);
+    }
+    singles.push(25);
+    singles.push(50);
+    return singles;
+  }
+
+  createDoublesArray(): number[]{
+    let i = 0;
+    let doubles = [];
+    for (i = 1; i <= 20; i++) {
+      doubles.push(i*2);
+    }
+    doubles.push(50);
+    return doubles;
+  }
+
+  createTriplesArray(): number[]{
+    let i = 0;
+    let triples = [];
+    for (i = 1; i <= 20; i++) {
+      triples.push(i*3);
+    }
+    return triples;
+  }
+
+
+  //Diese Funktion sollte nur aufgerufen werden, wenn Restliche Punkte >=170 sind
+  scoreToFinish(currentScore: number): void{ 
+      // Wenn CurrentScore >= 50 ist. Schaue nach, ob es in Double Array auftaucht
+  }
+
   // Hier wird player vermutlich gar nicht benötigt? Ich glaube es wird
   // viel mehr in den Spieler Setup gemacht und nach hier übergeben
   // Hier nur anzeige des Spieler Arrays und Berechnung für Average u Checkout?
